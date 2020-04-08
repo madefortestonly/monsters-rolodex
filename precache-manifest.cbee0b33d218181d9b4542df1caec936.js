a7cd8f1b8aff23d50ca2c3e44b0c0e7b8f592932
Madefor Testonly <greerherher@gmail.com>
2020-04-08 11:42:04 +0430

    Updates

---
+++ precache-manifest.cbee0b33d218181d9b4542df1caec936.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf87bc7d8df8634d6ad34c36ab75a310",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "745d3510946dec591cfb",
+    "url": "/monsters-rolodex/static/css/main.fca100a6.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "745d3510946dec591cfb",
+    "url": "/monsters-rolodex/static/js/main.6ba1422c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
